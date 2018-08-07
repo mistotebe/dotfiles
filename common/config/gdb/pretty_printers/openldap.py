4c07e07e0dacda4c39c127df7a141285df2a5181
Ondřej Kuzník <ondra@mistotebe.net>
2018-08-07 11:18:52 +0100

    OpenLDAP printers

--- common/config/gdb/pretty_printers/openldap.py
+++ common/config/gdb/pretty_printers/openldap.py
@@ -3,7 +3,9 @@
 import gdb
 import gdb.printing
 
-from pretty_printers.common import CollectionPrinter
+import socket
+
+from pretty_printers.common import CollectionPrinter, AnnotatedStructPrinter
 
 class LockPrinter:
     """Backup pretty printer for pthread_lock."""
@@ -31,7 +33,7 @@ class LockPrinter:
             return "Invalid"
         if self.locked:
             if self.kind == 1:
-                return "Locked by LWP {self.owner}, depth={self.lock}".format(self=self)
+                return "Locked by LWP {self.owner}, depth={self.locked}".format(self=self)
             else:
                 return "Locked by LWP {self.owner}".format(self=self)
         return "Unlocked"
@@ -139,8 +141,156 @@ class EntryPrinter:
         #    "dn": self.e['e_name'],
         }.items()
 
+class TaskPrinter:
+    """Pretty printer for ldap_int_thread_task_s"""
+
+    def __init__(self, work):
+        self.work = work
+
+    def to_string(self):
+        return self.work['ltt_start_routine']
+
+    def children(self):
+        return {
+            "arg": self.work['ltt_arg'],
+        }.items()
+
+class QueuePrinter:
+    """Pretty printer for ldap_int_thread_poolq_s"""
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def to_string(self):
+        return None
+
+    def children(self):
+        return {
+            "mutex": self.queue['ltp_mutex'],
+            #"cond": self.queue['ltp_cond'],
+            "work": self.queue['ltp_work_list']['stqh_first']
+        }.items()
+
+class PoolPrinter:
+    """Pretty printer for ldap_pvt_thread_pool_t"""
+
+    def __init__(self, pool):
+        self.pool = pool
+
+    def to_string(self):
+        state = self.pool['ltp_pause']
+        state_string = ""
+        if state:
+            state_string = "paused" if state == 2 else "pausing"
+
+        return "{}".format(state_string)
+
+    def children(self):
+        return {
+            "mutex": self.pool['ltp_mutex'],
+            #"cond": self.pool['ltp_cond'],
+            "queues": self.pool['ltp_numqs'],
+            "queue": self.pool['ltp_wqs'][0],
+        }.items()
+
+class DBPrinter(AnnotatedStructPrinter):
+    """Pretty printer for BackendDB"""
+    include = ['be_rootdn', 'be_next']
+
+    flags = {
+        'NOLASTMOD': 0x0001,
+        'NO_SCHEMA_CHECK': 0x0002,
+        'HIDDEN': 0x0004,
+        'ONE_SUFFIX': 0x0008,
+        'GLUE_INSTANCE': 0x0010,
+        'GLUE_SUBORDINATE': 0x0020,
+        'GLUE_LINKED': 0x0040,
+        'GLUE_ADVERTISE': 0x0080,
+        'OVERLAY': 0x0100,
+        'GLOBAL_OVERLAY': 0x0200,
+        'DYNAMIC': 0x0400,
+        'MONITORING': 0x0800,
+        'SHADOW': 0x8000,
+        'SINGLE_SHADOW': 0x4000,
+        'SYNC_SHADOW': 0x1000,
+        'SLURP_SHADOW': 0x2000,
+        'CLEAN': 0x10000,
+        'ACL_ADD': 0x20000,
+        'SYNC_SUBENTRY': 0x40000,
+        'MULTI_SHADOW': 0x80000,
+        'DISABLED': 0x100000,
+    }
+
+    def to_string(self):
+        if not self.value['bd_self']:
+            return "fake DB"
+        return self.value['be_suffix'][0]
+
+    def children(self):
+        if not self.value['bd_self']:
+            return []
+
+        result = self.children_dict(self.include)
+
+        result['type'] = self.value['bd_info']['bi_type']
+        result.move_to_end('type', last=False)
+
+        result['be_next'] = gdb.default_visualizer(result['be_next']['stqe_next']).to_string()
+
+        flags = self.value['be_flags']
+        if flags:
+            result['flags'] = []
+            for flag, value in self.flags.items():
+                if flags & value:
+                    result['flags'].append(flag)
+                    flags &= ~value
+            if flags:
+                result['flags'].append(str(flags))
+            result['flags'] = '|'.join(result['flags'])
+
+        return result.items()
+
+class OperationPrinter(AnnotatedStructPrinter):
+    """Pretty printer for Operation"""
+    exclude = ['o_next']
+
+    members = {
+        0x42: ["Unbind request", None],
+        0x4a: ["Delete request", None],
+        0x50: ["Abandon request", 'oq_abandon'],
+        0x60: ["Bind request", 'oq_bind'],
+        0x63: ["Search request", 'oq_search'],
+        0x66: ["Modify request", 'oq_modify'],
+        0x68: ["Add request", 'oq_add'],
+        0x6c: ["ModRDN request", 'oq_modrdn'],
+        0x6e: ["Compare request", 'oq_compare'],
+        0x77: ["Extended request", ''],
+    }
+
+    def to_string(self):
+        tag = int(self.value['o_tag'])
+        return self.members.get(tag, ['Unknown request: 0x{:x}'.format(tag)])[0]
+
+    def children(self):
+        result = self.children_dict()
+        tag = int(result.pop('o_tag'))
+
+        member = self.members.get(tag, ['', ''])[1]
+        if member is not None:
+            request = result['o_request']
+            if member:
+                result['o_request'] = request[member]
+        else:
+            del result['o_request']
+
+        return result.items()
+
 def finish_printer(printer):
-    mutex = gdb.lookup_type('pthread_mutex_t')
+    #mutex = gdb.lookup_type('pthread_mutex_t')
+
+    condition = gdb.lookup_type('pthread_cond_t')
+    printer.add_printer('condition', r'^ldap_pvt_thread_cond_t$',
+                        gdb.default_visualizer(condition))
 
 def register(objfile):
     print("registering OpenLDAP printers")
@@ -151,6 +301,8 @@ def register(objfile):
     printer.add_printer('mutex', r'^ldap_pvt_thread_mutex_t$',
                         LockPrinter)
     printer.add_printer('Sockaddr', r'^Sockaddr$', SockAddrPrinter)
+    printer.add_printer('ThreadPool', r'^ldap_pvt_thread_pool_t$',
+                        PoolPrinter)
 
     # pointer printers
     printer.add_pointer_printer('AttributeDescription',
@@ -160,6 +312,16 @@ def register(objfile):
                                 OCPrinter)
     printer.add_pointer_printer('Entry', r'^Entry$',
                                 EntryPrinter)
+
+    printer.add_pointer_printer('ThreadTask', r'^ldap_int_thread_task_s$',
+                                TaskPrinter)
+    printer.add_pointer_printer('ThreadPool', r'^ldap_int_thread_poolq_s$',
+                                QueuePrinter)
+
+    printer.add_pointer_printer('BackendDB', r'^BackendDB$',
+                                DBPrinter)
+    printer.add_pointer_printer('Operation', r'^Operation$',
+                                OperationPrinter)
 
     # register pthread aliases if possible
     gdb.post_event(finish_printer)
