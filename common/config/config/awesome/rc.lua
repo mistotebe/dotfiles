5dc8ba3212e12e18ff14962b194d3d3276f6253f
Ondřej Kuzník <ondra@mistotebe.net>
2019-08-29 13:15:47 +0100

    Syntax error

--- common/config/config/awesome/rc.lua
+++ common/config/config/awesome/rc.lua
@@ -395,7 +395,7 @@ globalkeys = gears.table.join(
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+              {description = "show the menubar", group = "launcher"}),
 
     -- Multimedia keys
     awful.key({                   }, "XF86AudioMute",        function () volume("toggle")   end),
